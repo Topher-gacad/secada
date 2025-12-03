db19d097465da994aad77d7d43fd2831f56ec37b
TopherGacad <gacadtophs@gmail.com>
2025-12-03 10:49:05 +0800

    feat: initial setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from config import Config
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route("/")
+def index():
+    return render_template("/auth/login.html")
+
+if __name__ == "__main__":
+    PORT=os.getenv("PORT")
+    app.run(debug=True, port=PORT)
