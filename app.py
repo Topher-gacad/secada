f549b66f0a3caff2148ea1212773397d2299698d
Tophergacad <gacadtophs@gmail.com>
2025-12-09 14:39:28 +0800

    fix: add migration init inside app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from models import db
 from config import Config
 import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db.init_app(app)
+migrate = Migrate(app, db)
+
+@app.route("/login")
+def login():
+    return render_template("auth/login.html")
+
 @app.route("/")
 def index():
-    return render_template("/auth/login.html")
+    return render_template("home/index.html")
 
 if __name__ == "__main__":
     PORT=os.getenv("PORT")
